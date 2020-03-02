35436cab49bc1d456fe266eb21d3c5d12b174cc3
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-03-02 13:34:25 +0800

    initialising components for landing page with static html content

---
+++ .history/client/src/App_20200302124046.js
@@ -0,0 +1,16 @@
+import React, {
+    Fragment
+} from 'react';
+//
+
+import './App.css';
+
+const App = () =>
+    //ghost element that won't show in the dom
+    <
+    Fragment >
+    <
+    h1 > App < /h1> <
+    /Fragment>
+
+export default App;
